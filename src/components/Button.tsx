dae05bf545259e083be6a159aa9730e81f0beb50
Yuri Oliveira <yurioliveira@aluno.fapce.edu.br>
2021-04-19 14:27:40 -0300

    style: criando tela de onbording

---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Text, TouchableOpacity, StyleSheet, TouchableOpacityProps } from 'react-native'
+import colors from '../styles/colors'
+
+interface ButtonProps extends TouchableOpacityProps {
+  title?: string
+}
+
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity 
+      style={styles.button}
+      activeOpacity={0.5}
+      {...rest}  
+    >
+      <Text> &gt; </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  }
+})
